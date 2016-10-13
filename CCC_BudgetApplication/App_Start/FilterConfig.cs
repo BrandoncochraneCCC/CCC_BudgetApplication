244444ca0b5ab6fa04668baf6629188ea5162b66
Brandon Cochrane <brandon.cochrane@calgarycounselling.com>
2016-10-13 12:47:28 -0600

    Add project files.

---
+++ CCC_BudgetApplication/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CCC_BudgetApplication
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ CCC_BudgetApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CCC_BudgetApplication.MvcApplication" Language="C#" %>
